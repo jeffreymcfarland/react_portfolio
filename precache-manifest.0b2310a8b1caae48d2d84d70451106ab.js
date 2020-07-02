1c3a4848ad6c7dc8eb4d80a374f6765085a8ebe8
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-07-01 21:24:56 -0500

    Updates

---
+++ precache-manifest.0b2310a8b1caae48d2d84d70451106ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c60d1cb54dc95bfb3006d5f68aa444f",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "b3ff6f16a9326235a9f0",
+    "url": "/react_portfolio/static/css/main.9634e0fa.chunk.css"
+  },
+  {
+    "revision": "19952f783f4742dc74a7",
+    "url": "/react_portfolio/static/js/2.1724d344.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react_portfolio/static/js/2.1724d344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ff6f16a9326235a9f0",
+    "url": "/react_portfolio/static/js/main.62722d2a.chunk.js"
+  },
+  {
+    "revision": "b30a7b2498e59c802f08",
+    "url": "/react_portfolio/static/js/runtime-main.04f95be2.js"
+  }
+]);
