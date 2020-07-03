965100bb1ec14a7063ee41da1045d6f57e239081
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-07-02 22:11:01 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e3eb470d4689290ac2f260e6713e669.js"
+  "/precache-manifest.128622932fffea4380b968d1dd48ad0f.js"
 );
 
 self.addEventListener('message', (event) => {
